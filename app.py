13e5858470a1c4c9d594c0ac4e0807f3b12f7308
Agus <agus@atadatos.es>
2018-04-21 16:50:50 +0200

    Fixed a bunch of typos that prevented the app to run properly

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+import os
 
 from resources.agent import Agent, AgentList
 from resources.server import Server, ServerList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('.', 'db' , 'test.db')
 api = Api(app)
 
 
@@ -23,4 +24,4 @@ api.add_resource(ServerList, '/servers')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
